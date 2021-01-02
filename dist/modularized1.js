6454e62482b3499af78a22ddfe2b14509ac7ff2f
jntc <jnthnc@protonmail.com>
2021-01-02 23:09:43 +0100

    cleaned code, added dropdown select for projects, empty project template, most functions good

--- dist/modularized1.js
+++ dist/modularized1.js
@@ -1,42 +1,71 @@
 var dom = {
+
     cacheDom: function () {
+
         //display areas
     this.projectDisplay = document.getElementById('project-display')
     this.taskDisplay = document.getElementById('task-display');
+
         //buttons
     this.submitButton = document.getElementById('submit');
     this.submitNewProject = document.getElementById('submit-new-button')
-            //delete buttons array
+
+            //delete buttons arrays
     this.deleteTaskButtons = Array.from(document.querySelectorAll("button.delete-task-button"));
     this.deleteProjectButtons = Array.from(document.querySelectorAll("button.delete-project-button"));
+
         //user inputs
             //tasks
+    this.projectSelect = document.getElementById('input-project');
     this.title = document.getElementById('input-title');
     this.description = document.getElementById('input-description');
     this.dueDate = document.getElementById('input-duedate');
     this.priority = document.getElementById('input-priority');
     this.notes = document.getElementById('input-notes');
-    this.project = document.getElementById('input-project');
             //project
-    this.newProject = document.getElementById('input-new-project')
+    this.newProject = document.getElementById('input-new-project');
     },
+
+    addListeners: function () {
+
+    this.submitNewProject.addEventListener('click', function () {
+        projects.submitAndUpdate();
+    }),
+    this.submitButton.addEventListener('click', function () {
+        tasks.submitAndUpdate();
+    })
+    },
+
     bindButtons: function () {
+
         this.deleteTaskButtons.forEach(button => {
             button.addEventListener('click', function() {
-                tasks.matchTasks(dom.getParentId(button)); //why dom and not this???
-            }, false)
+                tasks.matchTasks(button.parentElement.id);
+            })
         }),
+
         this.deleteProjectButtons.forEach(button => {
+
             button.addEventListener('click', function() {
-                //console.log(dom.getParentId(button))
-                projects.matchProjects(dom.getParentId(button))
+                projects.matchProjects(button.parentElement.id)
             })
         })
+
     },
-    getParentId: function (element) {
-        //console.log('sup')
-        return element.parentElement.id;
+
+    projectSelectTemplate: function (project) {
+        dom.projectSelect.insertAdjacentHTML('beforeend', 
+        `<option value="${project}">${project}</option>`)
     },
+
+    updateProjectSelect: function () {
+
+        dom.projectSelect.replaceChildren();
+        storage.projectArray.forEach(project => {
+            this.projectSelectTemplate(project)
+        })
+    },
+
     projectTemplate: function (project) {
         dom.projectDisplay.insertAdjacentHTML('beforeend', `<div class="project-wrapper">
         <div class="project-title" id="project-${project}" data-name=${project}>
@@ -52,6 +81,7 @@ var dom = {
         </div>
         </div>`)
     },
+
     taskTemplate: function (project, element) {
         var projectDiv = document.getElementById(`project-${project}`)
         projectDiv.insertAdjacentHTML('beforeend', `<div class="task-wrapper" id="${element.title + " " + project}">
@@ -64,67 +94,94 @@ var dom = {
         <div class="task-notes">${element.notes}</div>
     </div>`)
     },
-    clearProject: function (id) {
+
+    emptyTemplate: function (project) {
+        var projectDiv = document.getElementById(`project-${project}`);
+        projectDiv.insertAdjacentHTML('beforeend', `<div class="task-wrapper" id="empty + " " + ${project}">
+        <div class="task-counter">no tasks found for this project</div>
+    </div>`)
+    },
+
+    removeElementById: function (id) {
         var x = document.getElementById(id);
         x.remove();
-    },
-    alertChildren: function (node) {
-        node.querySelectorAll('div.project-title').forEach(child => {
-          alert(child.id);
-        })
     }
 }
 
 var projects = {
+
     init: function () {
         this.array = []; 
-        this.bindEvents()
-        this.createProjectsArray(); //fills this.array with all project names on dom
+        this.createProjectsArray(); //fills this.array with all project names on dom //should only fire once and probably in an if statement
 
                 //checking for storage
         if (localStorage.projects !== undefined) {
             this.array = storage.projectArray;
-            console.log('project storage found')
         } else {
             this.pushProject("my tasks");
-            console.log('NO project storage');
+            this.pushProject("test");
             storage.setProjectStorage();
             storage.setProjectArray();
         } 
     },
-    createProjectsArray: function () { //NEED TO USE STORAGE 
+
+    createProjectsArray: function () { //NEED TO USE STORAGE?
         //create array of all current projects on page, pushes name string into array
         var dataProjects = Array.from(document.querySelectorAll('.project-title'));
         dataProjects.forEach(el => {
             this.array.push(el.getAttribute('data-name'))
         })
     },
-    getProject: function() { //can probably move to dom object
-        const project = dom.newProject.value;
-        return project;
-    },
-    bindEvents: function () { //figure out best way to move this to DOM??? and same method in tasks object
-        dom.submitNewProject.addEventListener('click', this.submitAndUpdate.bind(this), false);
-    },
+
     submitAndUpdate: function () {
-        var name = this.getProject();
-        dom.projectTemplate(this.getProject());
-        //this.createProjectsArray();
-        this.pushProject(name);
-        storage.setProjectStorage();
-        storage.setProjectArray();
-        dom.cacheDom();
-        dom.bindButtons();
-        //tasks.loadTasks();
+
+        var name = dom.newProject.value;
+        if (!this.nameCheck(name)) {
+            dom.projectTemplate(name);
+            this.pushProject(name);
+            storage.setProjectStorage();
+            storage.setProjectArray();
+            dom.updateProjectSelect();
+            dom.cacheDom();
+            dom.bindButtons();
+            this.checkEmpties();
+        } else {
+            alert(`A project named ${name} already exists, please rename new project.`)
+        }
+
     },
+    checkEmpties: function () {
+
+        storage.projectArray.forEach(element => {
+            if (!this.taskInProject(element) && !this.hasEmptyMsg(element)) {
+                console.log('yes')
+                dom.emptyTemplate(element)
+            }
+        })
+    },
+
     loadProjects: function () {
         storage.projectArray.forEach(element => {
-                dom.projectTemplate(element)
+            dom.projectTemplate(element)
         })
+        this.checkEmpties();
+    },
+    
+    taskInProject: function (project) {
+        return storage.taskArray.some(task => task.project == project)
+    },
+    hasEmptyMsg: function (project) {
+
+        var childArray = Array.from(document.getElementById(`project-${project}`).childNodes);
+        return childArray.some(p => p.id == 'empty + ')
+    },
+    nameCheck: function (name) {
+       return storage.projectArray.some(x => x == name);
     },
     pushProject: function (project) {
         this.array.push(project);
         },
+    
     matchProjects: function (id) {
         
         storage.projectArray.forEach(item => {
@@ -132,36 +189,39 @@ var projects = {
                 alert(`This will delete your project '${item}' and all associated tasks. Press ok to confirm.`)
                 tasks.removeTasksInProject(item);
                 storage.projectArray.splice(storage.projectArray.indexOf(item), 1);
-                dom.clearProject(id)//need to make general dom method or specific
+                dom.removeElementById(id)
                 this.array = storage.projectArray;
                 storage.setProjectStorage();
-                //this.loadProjects();
-                //tasks.loadTasks(); //this shouldn't be here
+                dom.updateProjectSelect();
                 dom.cacheDom();
                 dom.bindButtons();
-             } else {
-                 //alert('nope')
-             }
+                this.checkEmpties();
+
+             } 
         })
     }
 }
+
 var tasks = {
     init: function () {
-        this.array = [];
-        this.bindEvents();
-        storage.init(); //should this be here or in global?
 
+        this.array = [];
+        storage.init(); //should this be here or in global?
+        
         //checking for storage
         if (localStorage.tasks !== undefined) {
             this.array = storage.taskArray;
             console.log('task storage found')
         } else {
             this.pushTask(this.createTask({title: 'create a new task', description: 'this is the first thing on your list!', project: "my tasks"}));
+            this.pushTask(this.createTask({title: 'test', description: 'test', project: "test"}));
             console.log('NO task storage');
             storage.setTaskStorage();
             storage.setTaskArray();
+
         } 
     },
+
     getObject: function () { //getObject is argument for createTask //move to dom object??
         const taskObject = {
           title: dom.title.value,
@@ -169,10 +229,11 @@ var tasks = {
           dueDate: dom.dueDate.value,
           priority: dom.priority.value,
           notes: dom.notes.value,
-          project: dom.project.value
+          project: dom.projectSelect.value
         }
         return taskObject
     },
+
     createTask: function ({title, description, dueDate, priority, notes, project}) {
        return {
         title,
@@ -184,25 +245,35 @@ var tasks = {
         counter: this.array.length + 1,
       }
     },
+
+    titleCheck: function (task) {
+        return storage.taskArray.some(x => (x.title == task.title) && (x.project == task.project))
+    },
+
     pushTask: function (task) {
     this.array.push(task);
     },
+
     submitAndUpdate: function () {
-        if (localStorage.tasks == undefined) {                //******something fucky here here innit
-            this.array = [];
-        }
-      this.pushTask(this.createTask(this.getObject()));
-      this.clear(dom.projectDisplay)
-      storage.setTaskStorage();
-      storage.setTaskArray();
-      this.loadTasks();
-      dom.cacheDom()
-      dom.bindButtons();
+      var task = this.createTask(this.getObject());
+
+      if (!this.titleCheck(task)) {
+        this.pushTask(task);
+        this.clear(dom.projectDisplay)
+        storage.setTaskStorage();
+        storage.setTaskArray();
+        this.loadTasks();
+        dom.cacheDom()
+        dom.bindButtons();
+        projects.checkEmpties()
+      } else {
+          alert(`A task with this title already exists under project ${task.project}.`)
+      }
     },
+
     matchTasks: function (id) {
         storage.taskArray.forEach(item => {
             if (id == (item.title + " " + item.project)) {
-                //alert('yes')
                 storage.taskArray.splice(storage.taskArray.indexOf(item), 1);
                 this.clear(dom.projectDisplay)
                 this.array = storage.taskArray;
@@ -210,25 +281,24 @@ var tasks = {
                 this.loadTasks();
                 dom.cacheDom();
                 dom.bindButtons();
-            } else {
-               //alert('nope')
-            }
+                projects.checkEmpties()
+            } 
         })
     },
-    bindEvents: function () {
-      dom.submitButton.addEventListener('click', this.submitAndUpdate.bind(this), false);
-    },
+
     clear: function (node) {
       node.querySelectorAll('div.task-wrapper').forEach(child => {
         child.remove();
       })
     },
+
     loadTasks: function () {
         storage.taskArray.forEach(element => {
                 var project = element.project
                 dom.taskTemplate(project, element)
         })
     },
+
     removeTasksInProject: function (item) {
         storage.taskArray.forEach(element => {
             if (element.project == item) {
@@ -241,6 +311,7 @@ var tasks = {
 }
 
 var storage = {
+
     init: function () {
         this.serializer();
         if (localStorage.tasks !== undefined) {
@@ -250,7 +321,8 @@ var storage = {
             this.taskArray = [];
             this.projectArray =[];
         }
-        },
+    },
+
     serializer: function () {
         Storage.prototype.setObj = function(key, obj) {
             return this.setItem(key, JSON.stringify(obj))
@@ -259,40 +331,50 @@ var storage = {
             return JSON.parse(this.getItem(key))
         }
     },
+
     setTaskStorage: function () {
         localStorage.setObj('tasks', tasks.array)
     },
+
     setProjectStorage: function () {
         localStorage.setObj('projects', projects.array)
     },
-    setTaskArray: function () {
+
+    setTaskArray: function () { //change these to GET not set
         this.taskArray = localStorage.getObj('tasks');
     },
+
     setProjectArray: function () {
         this.projectArray = localStorage.getObj('projects');
     },
+
     clear: function () {
         window.localStorage.clear();
         this.taskArray = [];
         this.projectArray = [];
     }
 }
-
-
-
-
-
-
-                                        //****WORKING ON */
-
                                                                     //init
                                                                     dom.cacheDom();
                                                                     tasks.init();
                                                                     projects.init();
                                                                     projects.loadProjects()
                                                                     tasks.loadTasks();
+                                                                    dom.addListeners();
+                                                                    dom.updateProjectSelect();
                                                                     dom.cacheDom();
                                                                     dom.bindButtons();
+
+                                                                    
+
+
+
+                                        //****WORKING ON */
+                        //project dropdown in add task
+                        
+//NOTES : 
+//some projects are remaining in dropdown after deleted
+//strange, when no projects are on dom 'add' project will clear some from dropdown, can't nail it down
 
 
 
@@ -312,7 +394,32 @@ var storage = {
 //         //console.log(array)
 //     })
 //     }
-//#endregion
 
-//NOTES : 
-//delete project works now, but now sometimes can't add new tasks under new project after deleting projects
+// const setUp = function() {
+
+//     projects.pushProject("testing");
+//     projects.loadProjects()
+//     tasks.pushTask(tasks.createTask({title: 'set', description: 'set', project: "set"}));
+//     tasks.loadTasks()
+//     storage.setTaskStorage();
+//     storage.setTaskArray();
+//     storage.setProjectStorage();
+//     storage.setProjectArray();
+//     dom.cacheDom();
+//     dom.bindButtons();
+    
+// }
+
+
+                // if(this.containsTask(button)) {
+                //     console.log('yes')
+                    
+                // } else {
+                //     console.log('no')
+                //     button.addEventListener('click', function() {
+                //             console.log('fired')
+                //             tasks.matchTasks(dom.getParentId(button));
+                //         })
+                //     }
+                
+//#endregion
